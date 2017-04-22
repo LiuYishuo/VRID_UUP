9fd072bd9c1220eaef6cb5a19eea11ee5de92770
Wellyanto <wellyan2@illinois.edu>
2017-04-22 18:10:48 -0500

    added selector functionalities

--- VRID_Oculus_UUP/Assets/_Scripts/MainMenuController.cs
+++ VRID_Oculus_UUP/Assets/_Scripts/MainMenuController.cs
@@ -9,13 +9,18 @@ public class MainMenuController : MonoBehaviour {
 
     private Transform[] categories;
     private Transform[] contents;
+    private Transform selector;
 
     // this is for the highlighter/cursor/selector
     private int selector_x = 0;
     private int selector_y = 0;
 
+    // this is the dimension of the grid
+    private int GRID_X = 5;
+    private int GRID_Y = 4;
+
     // Use this for initialization
-    void Start () {
+    void Start() {
         // instantiate the private parts
         currentCategory = Category.Beds;
 
@@ -24,6 +29,16 @@ public class MainMenuController : MonoBehaviour {
 
         // populate the furniture
         populateObjects();
+
+        // get the selector
+        setSelector();
+    }
+
+    private void setSelector()
+    {
+        this.selector = this.gameObject.transform.Find("Selector");
+        
+        highlightSelection();
     }
 
     private void setCategoriesContents()
@@ -41,11 +56,86 @@ public class MainMenuController : MonoBehaviour {
         
         // automatic highlighting of the category
         highlightCategory();
+        
+        // update the location of the selector
+        updateSelection();
 
-        // highlight the current selection depicted by the selector
+        // when user select a specific furniture inside the grid
+        selectFurniture();
     }
 
-    // TODO make the content change when the category changes
+    // TODO When the user pressed A. find the current object selected. and render the object
+    private void selectFurniture()
+    {
+        // TODO. still not working
+        if (OVRInput.GetUp(OVRInput.Button.One))
+        {
+            Transform found = null;
+
+            int x = selector_x * 200 - 400;
+            int y = selector_y * -100 + 150;
+            Vector3 position = new Vector3(x, y - 50, 0);
+
+            Transform[] furnitureTiles = getFirstChildren(contents[(int)currentCategory].gameObject.transform);
+            foreach (Transform item in furnitureTiles)
+                if (item.GetComponent<RectTransform>().transform.localPosition == position)
+            {
+                //if (item.GetComponent<RectTransform>().transform.localPosition == position)
+                if (item.localPosition == position)
+                {
+                    Debug.Log("found!");
+                    found = item;
+                    break;
+                }
+            }
+
+            Debug.Log(found);
+        }
+    }
+
+    // This updates the coordinate of the selected item based on user's dpad
+    private void updateSelection()
+    {
+        if (OVRInput.GetUp(OVRInput.Button.DpadUp) && selector_y >= 1)
+        {
+            selector_y--;
+
+            highlightSelection();
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.DpadDown) && selector_y < (GRID_Y - 1))
+        {
+            selector_y++;
+
+            highlightSelection();
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.DpadLeft) && selector_x >= 1)
+        {
+            selector_x--;
+
+            highlightSelection();
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.DpadRight) && selector_x < (GRID_X - 1))
+        {
+            selector_x++;
+
+            highlightSelection();
+        }
+    }
+
+    // This updates the models based on the coordinate of the selectors
+    private void highlightSelection()
+    {
+        // move the location of the selector
+        int x = selector_x * 200 - 400;
+        int y = selector_y * -100 + 150;
+
+        this.selector.localPosition = new Vector3(x, y-50, 0);
+    }
+
+    // make the content change when the category changes
     private void updateContent()
     {      
         for (int i = 0; i < contents.Length; i++)
@@ -57,10 +147,12 @@ public class MainMenuController : MonoBehaviour {
 
     private void updateCategory()
     {
+        int enumLength = Category.GetNames(typeof(Category)).Length;
+
         // Left Bumper
         if (Input.GetKeyUp("joystick button 4"))
         {
-            int index = (((int) currentCategory - 1) % 4 + 4) % 4;
+            int index = (((int) currentCategory - 1) % enumLength + enumLength) % enumLength;
             currentCategory = (Category) index;
             updateContent();
         }
@@ -68,10 +160,9 @@ public class MainMenuController : MonoBehaviour {
         // Right Bumper
         if (Input.GetKeyUp("joystick button 5"))
         {
-            currentCategory = (Category)((int)(currentCategory + 1) % 4);
+            currentCategory = (Category)((int)(currentCategory + 1) % enumLength);
             updateContent();
         }
-
     }
 
     // this function should populate the furnitures of the current selected category
